3efc42be0dcab2e5d1e4f9db0f5ffc640a85bd32
joshua.haydonguppy@gmail.com <R3adytolean>
2025-01-03 02:12:05 +0000

    ImageUtils - added ImageUtils to utils with an extension of its base class

---
+++ UltraPowerMode/UltraPowerMode/Utils/ImageUtils.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing;
+using Image = System.Windows.Controls.Image;
+
+namespace UltraPowerMode.Utils
+{
+    public static class ImageUtils
+    {
+        public static void UpdateSource(this Image image, Bitmap bitmap)
+        {
+            var bitmapImage = new System.Windows.Media.Imaging.BitmapImage();
+            using (var memory = new System.IO.MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Png);
+                memory.Position = 0;
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = System.Windows.Media.Imaging.BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+            }
+            image.Source = bitmapImage;
+        }
+    }
+}
